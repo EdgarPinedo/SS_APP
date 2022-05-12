14c0f5275bc33e4d2dbc695a20e425f906046e85
EdgarPinedo <edgarpinedo122@hotmail.com>
2022-05-12 12:41:07 -0500

    tabla de materias

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import json
 
 cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
@@ -27,14 +28,29 @@ db = firestore.client()
 # for materia in materias:
 #     print(materia.to_dict())
 
+materias = db.collection('Materias').get()
+list = []
+for materia in materias:
+    list.append(materia.to_dict())
+
+#jsonToString = json.dumps(materia)
+#jsonload = json.loads(jsonToString)
+print(list)
+
 from flask import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    materia = db.collection('Materias').document('Algoritmia').get()
-    return materia.to_dict()
+    materias = db.collection('Materias').get()
+    #materias = materias.to_dict()
+    #print(materias)
+    list = []
+    for materia in materias:
+        list.append(materia.to_dict())
+    return render_template('index.html', mats = list)
+    #return materia['id']
 
 
 if __name__ == '__main__':
