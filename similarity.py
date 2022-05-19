8b08f19927c75717f1c6508163b4be8fc0120fa8
Jesus <jesuse.salas@alumnos.udg.mx>
2022-05-18 19:28:11 -0500

    Similitud pt 1

---
+++ similarity.py
@@ -0,0 +1,32 @@
+import string
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+
+stopwords = stopwords.words('spanish')
+
+def cleanString(text):
+    text = "".join([char for char in text if char not in string.punctuation])
+    text = text.lower()
+    text = " ".join([word for word in text.split() if word not in stopwords])
+    return text
+
+def cosineSimVectors(vec1, vec2):
+    vec1 = vec1.reshape(1, -1)
+    vec2 = vec2.reshape(1, -1)
+
+    return cosine_similarity(vec1, vec2)[0][0]
+
+def main(): 
+    strings = ["Programación", "Estrucrura de Datos"]
+    cleaned = list(map(cleanString, strings))
+    print(cleaned)
+    vectorizer = CountVectorizer().fit_transform(cleaned)
+    vectors =  vectorizer.toarray()
+    csim = cosineSimVectors(vectors[0], vectors[1])
+    print(csim)
+    
+
+
+if __name__ == "__main__":
+    main()
